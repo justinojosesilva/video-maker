a46b23535f807329963cf5f6b6a3e7229aaf5547
Justino Jose <justinojosesilva@gmail.com>
2020-02-19 23:41:37 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/images.js')
 }
 
 
 async function start() {
 
-    robots.input()
-    await robots.text()
+    //robots.input()
+    //await robots.text()
+    await robots.image()
     
     const content = robots.state.load()
     
